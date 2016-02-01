f9718f67baadffe4b482361653b4dba0560aa8fd
JB Poline <jbpoline@gmail.com>
2016-01-31 22:39:21 -0800

    more renames of json model files, update copy script

--- bids2pype/examples_models_n_spec/models/cp_models_to_data.sh
+++ bids2pype/examples_models_n_spec/models/cp_models_to_data.sh
@@ -2,10 +2,12 @@
 
 echo `pwd`
 bidsdata='/home/jb/data/bids'
-echo "cp ds-005_model.json "$bidsdata"/ds005/models"
-cp ds-005_model.json $bidsdata"/ds005/models"
+echo "cp ds-005*_model.json "$bidsdata"/ds005/models"
+cp ds-005_*model.json $bidsdata"/ds005/models"
 echo "cp ds-105_level-all_model.json" $bidsdata"/ds105/models"
 cp ds-105_level-all_model.json $bidsdata"/ds105/models"
-echo "ds-105_sub-2_model.json" $bidsdata"/ds005/sub-2/models"
-cp ds-105_sub-2_model.json $bidsdata"/ds105/sub-2/models"
+echo "cp ds-105_level-run_model.json" $bidsdata"/ds105/models"
+cp ds-105_level-run_model.json $bidsdata"/ds105/models"
+echo "ds-105_level-run_sub-2_model.json" $bidsdata"/ds005/sub-2/models"
+cp ds-105_level-run_sub-2_model.json $bidsdata"/ds105/sub-2/models"
 
