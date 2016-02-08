7cfccb774a067426245bc9fe5a191dfb73c22197
JB Poline <jbpoline@gmail.com>
2016-02-07 16:56:59 -0800

    typo in copy model files script

--- bids2pype/examples_models_n_spec/models/cp_models_to_data.sh
+++ bids2pype/examples_models_n_spec/models/cp_models_to_data.sh
@@ -9,5 +9,8 @@ cp ds-105_level-all_model.json $bidsdata"/ds105/models"
 echo "cp ds-105_level-run_model.json" $bidsdata"/ds105/models"
 cp ds-105_level-run_model.json $bidsdata"/ds105/models"
 echo "ds-105_level-run_sub-2_model.json" $bidsdata"/ds005/sub-2/models"
-cp ds-105_level-run_sub-2_model.json $bidsdata"/ds105/sub-2/models"
+cp ds-105_level-run_sub-2_model.json $bidsdata"/ds105/sub-2/func/models"
 
+
+# /home/jb/code/bids2pype/bids2pype/examples_models_n_spec/models/ds-105_level-run_sub-2_model.json
+         
